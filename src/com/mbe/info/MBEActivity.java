bf003e0f287587a94e0ca72d2f83558af56ab2d6
DESKTOP-7JRTRPT\Sandli <sandlimatharoo24@gmail.com>
2017-02-12 11:05:33 +0530

    Project phase-1 checkin

---
+++ src/com/mbe/info/MBEActivity.java
@@ -0,0 +1,19 @@
+package com.mbe.info;
+
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MBEActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.
+	}
+
+	
+}
